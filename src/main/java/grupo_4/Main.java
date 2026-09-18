d54e041206dcca53039f9d8042b8a639f74da8dd
Davis_0n <Davis@DESKTOP-HCJRUHU>
2026-09-17 21:35:08 -0600

    feat: Main arranca JavaFX y abre la ventana de acceso

---
+++ src/main/java/grupo_4/Main.java
@@ -0,0 +1,24 @@
+package grupo_4;
+
+import grupo_4.clientes.Ventanas;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/grupo_4/view/login.fxml"));
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Gestion de clientes | Acceso");
+        stage.setResizable(false);
+        Ventanas.confirmarCierre(stage);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
